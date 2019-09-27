3c62ec15641737f5e6d70d5da5b0f15f56e56317
Wenyi <wenyiccchu@gmail.com>
2019-09-26 20:40:24 -0400

    ready for submission

--- titanic.py
+++ titanic.py
@@ -14,49 +14,44 @@ import matplotlib.pyplot as plt
 
 from sklearn.linear_model import LogisticRegression
 
-train_df = pd.read_csv('./titanic/train.csv')
-test_df = pd.read_csv('./titanic/test.csv')
-combine = [train_df, test_df]
+trainSet = pd.read_csv('./titanic/train.csv')
+testSet = pd.read_csv('./titanic/test.csv')
+combine = [trainSet, testSet]
 
-#print(train_df.columns.values)
-#train_df.describe()
+#print(trainSet.columns.values)
+#trainSet.describe()
 
 def prepare_data():
     # examine some features (exploring correlations)
-    #train_df[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean()
-    #train_df[["Sex", "Survived"]].groupby(['Sex'], as_index=False).mean()
+    #trainSet[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean()
+    #trainSet[["Sex", "Survived"]].groupby(['Sex'], as_index=False).mean()
     
-    #g = sns.FacetGrid(train_df, col='Survived')
+    #g = sns.FacetGrid(trainSet, col='Survived')
     #g.map(plt.hist, 'Pclass', bins = 10, alpha = .5, color="lightblue")
     #g.add_legend()
     #
-    #g2 = sns.FacetGrid(train_df, row='Embarked', col='Survived', size=2.2, aspect=1.6)
+    #g2 = sns.FacetGrid(trainSet, row='Embarked', col='Survived', size=2.2, aspect=1.6)
     #g2.map(sns.barplot, 'Sex', 'Fare', alpha=.5)
     #g2.add_legend()
     
-    combine = [train_df, test_df]
+    combine = [trainSet, testSet]
     
     # fill in empty values for Embarked feature (using majority vote)
-    freq_port = train_df.Embarked.dropna().mode()[0]
+    freq_port = trainSet.Embarked.dropna().mode()[0]
     
     for dataset in combine:
         dataset['Embarked'] = dataset['Embarked'].fillna(freq_port)
         
-    # converted Age feature into a numerical (int)
-    for dataset in combine:
+        # converted Age feature into a numerical (int)
         dataset['Embarked'] = dataset['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
-        
     
-    # converted Age feature into a numerical (int)
-    for dataset in combine:
+        # converted Age feature into a numerical (int)
         dataset['Sex'] = dataset['Sex'].map( {'female': 1, 'male': 0} ).astype(int)
-        
-        
-    # fill in empty values for Age feature (using median)
-    for dataset in combine:
+          
+        # fill in empty values for Age feature (using median)
         dataset['Age'] = dataset['Age'].fillna(dataset['Age'].mean())
 
-    #train_df.head()
+    #trainSet.head()
     
     # converted Age feature into a categorical (int)
     for dataset in combine:    
@@ -66,47 +61,43 @@ def prepare_data():
         dataset.loc[(dataset['Age'] > 40) & (dataset['Age'] <= 50), 'Age'] = 3
         dataset.loc[(dataset['Age'] > 50) & (dataset['Age'] <= 60), 'Age'] = 4
         dataset.loc[ dataset['Age'] > 60, 'Age']
-    #train_df.head()
+    #trainSet.head()
     
-    
-    # created a new feature (if travel alone) by combining sibsp and parch
-    for dataset in combine:
-        dataset['Family'] = dataset['SibSp'] + dataset['Parch'] + 1
     
     for dataset in combine:
+        # created a new feature (if travel alone) by combining sibsp and parch
+        dataset['Family'] = 1 + dataset['SibSp'] + dataset['Parch']  
         dataset['IsAlone'] = 0
         dataset.loc[dataset['Family'] == 1, 'IsAlone'] = 1
     
-    # fill in empty values for Fare feature (using median)
-    for dataset in combine:
-        test_df['Fare'].fillna(test_df['Fare'].dropna().median(), inplace=True)
+        # fill in empty values for Fare feature (using median)
+        testSet['Fare'].fillna(testSet['Fare'].dropna().median(), inplace=True)
         
-    #train_df.head()
-    #test_df.info()
-
+    #trainSet.head()
+    #testSet.info()
 
 
 prepare_data()
 
 # ignore unnecessary features
-train_df = train_df.drop(['Ticket', 'Cabin'], axis=1)
-test_df = test_df.drop(['Ticket', 'Cabin'], axis=1)
-train_df = train_df.drop(['Parch','SibSp','Family'], axis=1)
-test_df = test_df.drop(['Parch','SibSp','Family'], axis=1)
-train_df = train_df.drop(['Name', 'PassengerId'], axis=1)
-test_df = test_df.drop(['Name'], axis=1)
+trainSet = trainSet.drop(['Ticket', 'Cabin'], axis=1)
+testSet = testSet.drop(['Ticket', 'Cabin'], axis=1)
+trainSet = trainSet.drop(['Parch','SibSp','Family'], axis=1)
+testSet = testSet.drop(['Parch','SibSp','Family'], axis=1)
+trainSet = trainSet.drop(['Name', 'PassengerId'], axis=1)
+testSet = testSet.drop(['Name'], axis=1)
     
-train_label = train_df["Survived"]
-train = train_df.drop("Survived", axis=1)
-passengerId = test_df['PassengerId']
-test  = test_df.drop("PassengerId", axis=1)
+trainLabel = trainSet["Survived"]
+train = trainSet.drop("Survived", axis=1)
+passengerId = testSet['PassengerId']
+test  = testSet.drop("PassengerId", axis=1)
     
-#train_label.shape, train.shape, test.shape
+#trainLabel.shape, train.shape, test.shape
     
     
 # use sklearn's logisticRegresssion to train dataset
 logreg = LogisticRegression()
-logreg.fit(train, train_label)
+logreg.fit(train, trainLabel)
 pred = logreg.predict(test)
 
 out = pd.DataFrame(columns=['PassengerId', 'Survived'])
